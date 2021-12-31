855e76af30e5e9ac0007041bf8dedf0a8ffdebeb
Mahdieh <mahdeh.abed@gmail.com>
2021-12-31 22:23:49 +0330

    check items done

--- src/Components/ToDoItems/ToDoItems.jsx
+++ src/Components/ToDoItems/ToDoItems.jsx
@@ -1,12 +1,12 @@
 import "./ToDoItems.css";
 
-import {Checkbox } from "@mui/material";
+import { Checkbox } from "@mui/material";
 
 import RadioButtonCheckedRoundedIcon from "@mui/icons-material/RadioButtonCheckedRounded";
 import RadioButtonUncheckedRoundedIcon from "@mui/icons-material/RadioButtonUncheckedRounded";
 import HighlightOffRoundedIcon from "@mui/icons-material/HighlightOffRounded";
 
-const ToDoItems = ({ list, toDoList, setToDoList}) => {
+const ToDoItems = ({ list, toDoList, setToDoList }) => {
   const handleDeleteItem = (listId, ItemId) => {
     setToDoList(
       toDoList.map((list) =>
@@ -19,7 +19,22 @@ const ToDoItems = ({ list, toDoList, setToDoList}) => {
       )
     );
   };
-
+  const handleUpdateTodoItemStatus = (listId, itemId, e) => {
+    setToDoList(
+      toDoList.map((todo) =>
+        todo.listID === listId
+          ? {
+              ...todo,
+              listItem: todo.listItem.map((item) =>
+                item.itemID === itemId
+                  ? { ...item, status: e.target.checked }
+                  : item
+              ),
+            }
+          : todo
+      )
+    );
+  };
   return (
     <>
       {list.listItem.map((item) => (
@@ -27,6 +42,10 @@ const ToDoItems = ({ list, toDoList, setToDoList}) => {
           <div key={`${list.listID}-${item.id}`}>
             <Checkbox
               icon={<RadioButtonUncheckedRoundedIcon fontSize="large" />}
+              checked={item.status}
+              onChange={(e) =>
+                handleUpdateTodoItemStatus(list.listID, item.itemID, e)
+              }
               checkedIcon={
                 <RadioButtonCheckedRoundedIcon
                   color="success"
